bdde989f88ce0642808f955cfb6e08b5b594bcfa
0b1111001 <anderusman2004@gmail.com>
2024-06-22 00:45:05 +0700

    Modify watermelon.cpp

--- watermelon.cpp
+++ watermelon.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+void solve(){
     int n;
     cin>>n;
-    cout<<((n%2==0 && n!=2) ? "YES" : "NO")<<endl;;
+    cout<<((n%2==0 && n!=2) ? "YES" : "NO")<<endl;
+}
+
+int main(){
+    solve();
 }
