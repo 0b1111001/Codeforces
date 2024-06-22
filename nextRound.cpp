7eef42e13fc78c6773edf7f718705342300969bf
0b1111001 <anderusman2004@gmail.com>
2024-06-22 14:19:55 +0700

    Add nextRound.cpp

---
+++ nextRound.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void solve(){
+    int n,k,pass(0),last(-1);
+    cin>>n>>k;
+    while(n--){
+        int a;
+        cin>>a;
+        if(k==1) last=a;
+        if(k>0) pass++;
+        else{
+            if(a==last) pass++;
+            else break;
+        }
+        k--;
+    }
+    cout<<pass<<endl;
+}
+
+int main(){
+    solve();
+    return 0;
+}
